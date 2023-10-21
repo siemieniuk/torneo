c865e7cf95116a79d94c32c74af6685a8b4db550
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2023-10-21 13:50:11 +0200

    custom Django messages

--- app/mysite/settings.py
+++ app/mysite/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from django.contrib.messages import constants as messages
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -152,3 +154,13 @@ LOGOUT_REDIRECT_URL = "/accounts/login"
 # Media
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "media/"
+
+# Messages configuration
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
