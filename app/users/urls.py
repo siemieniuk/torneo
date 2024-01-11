1fbd7ed8de1c05a036d689da72c6b33526df31ab
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2024-01-11 10:12:23 +0100

    Activation email

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,9 +1,14 @@
 from django.contrib.auth import views as auth_views
 from django.urls import include, path
 
-from users.views import register_view
+from users.views import activate, register_view
 
 urlpatterns = [
     path("register/", register_view, name="register"),
     path("", include("django.contrib.auth.urls")),
+    path(
+        "activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/",
+        activate,
+        name="activate",
+    ),
 ]
--- app/users/views.py
+++ app/users/views.py
@@ -1,14 +1,23 @@
-from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.contrib.auth import login
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes, force_str
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 
 from users.forms import CustomUserCreationForm
+from users.models import MyUser
+from users.tokens import account_activation_token
 
 
 def register_view(request):
     match (request.method):
         case "GET":
             if request.user.is_authenticated:
-                return redirect("index")
+                return redirect("my_page")
             return render(
                 request,
                 "registration/register.html",
@@ -17,6 +26,43 @@ def register_view(request):
         case "POST":
             form = CustomUserCreationForm(request.POST)
             if form.is_valid():
-                user = form.save()
-                login(request, user)
-                return redirect("index")
+                user = form.save(commit=False)
+                user.is_active = False
+                user.save()
+
+                current_site = get_current_site(request)
+                mail_subject = "Torneo: Activation link"
+                message = render_to_string(
+                    "activation_mail.html",
+                    {
+                        "user": user,
+                        "domain": current_site.domain,
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "token": account_activation_token.make_token(user),
+                    },
+                )
+                recipient = form.cleaned_data.get("email")
+                email = EmailMessage(mail_subject, message, to=[recipient])
+                email.send()
+                return render(
+                    request,
+                    "registration/activation_mail_sent.html",
+                    {"email": user.email},
+                )
+
+
+def activate(request, uidb64, token):
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = MyUser.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, MyUser.DoesNotExist):
+        user = None
+    if user is not None and account_activation_token.check_token(user, token):
+        user.is_active = True
+        user.save()
+        login(request, user)
+        messages.success(request, "Successfully confirmed your email account!")
+        return redirect("my_page")
+    else:
+        messages.error(request, "Activation link is invalid!")
+        return redirect("login")
