f92affd70db7fc44d04d09117ed81d245c06f278
Szymon Siemieniuk <36564363+siemieniuk@users.noreply.github.com>
2024-01-11 10:10:33 +0100

    Remove junk, commented code

--- app/torneo_app/admin.py
+++ app/torneo_app/admin.py
@@ -2,15 +2,10 @@ from django.contrib import admin
 
 from torneo_app.models import *
 from users.admin import CustomUserAdmin
+from users.models import MyUser
 
-# admin.site.register(Discipline, CustomUserAdmin)
-# admin.site.register(Tournament, CustomUserAdmin)
-# admin.site.register(Sponsor, CustomUserAdmin)
-# admin.site.register(Result, CustomUserAdmin)
-# admin.site.register(TournamentAssignment, CustomUserAdmin)
-
-admin.site.register(Discipline)
 admin.site.register(Tournament)
+admin.site.register(Discipline)
 admin.site.register(Sponsor)
 admin.site.register(Result)
 admin.site.register(TournamentAssignment)
